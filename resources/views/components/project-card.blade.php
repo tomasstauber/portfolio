45975ab9d632b9e9f4e5041bb89b186c8a488eea
Tomas Stauber <tstauberalumno@itec-elmolino.edu.ar>
2026-04-29 17:43:25 -0300

    Refactor: projects page

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -1,13 +1,27 @@
 <div class="project-card">
-    <h3>// {{ $title }}</h3>
+    <div class="project-card__content">
+        <h3>// {{ $title }}</h3>
 
-    <p>{{ $description }}</p>
+        <p>{{ $description }}</p>
 
-    <div class="card-tags">
-        @foreach($tags as $tag)
-            <span>{{ $tag }}</span>
-        @endforeach
+        <div class="card-tags">
+            @foreach($tags as $tag)
+                <span>{{ $tag }}</span>
+            @endforeach
+        </div>
     </div>
 
-    <a href="{{ $link }}">Ver proyecto</a>
+    <div class="card-links">
+        @if(!empty($link) && $link !== '#')
+            <a href="{{ $link }}" target="_blank" rel="noopener noreferrer">
+                Ver proyecto
+            </a>
+        @endif
+
+        @if(!empty($repo) && $repo !== '#')
+            <a href="{{ $repo }}" target="_blank" rel="noopener noreferrer">
+                Repositorio
+            </a>
+        @endif
+    </div>
 </div>
